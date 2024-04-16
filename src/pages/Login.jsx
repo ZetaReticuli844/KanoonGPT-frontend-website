505ab7bf3304e3f83e2b311a6aacce8ad9835e86
Yogesh <awesomeyogi10@gmail.com>
2024-04-16 08:23:06 +0530

    added Login and register pages

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Signin from '../components/SignIn'
+
+const Login = () => {
+  return (
+    <div>
+      <Signin/>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Signup from '../components/SignUp'
+
+const Register = () => {
+  return (
+    <div>
+      <Signup/>
+    </div>
+  )
+}
+
+export default Register
